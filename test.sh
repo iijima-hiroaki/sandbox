298bc87543e35813d30dc819476469f3d55c15a8
hiijima <42027082+iijima-hiroaki@users.noreply.github.com>
2022-01-13 21:54:26 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,14 +10,4 @@ while true; do
   esac
 done
 
-while true; do
-  echo test2
-  read -p "[test2] please yes or no. (y/N): " yn
-  case $yn in
-    [Yy]* ) break ;;
-    [Nn]* ) printf $RED "[test2] 処理を中断します." && exit 1;;
-    * ) echo '[test2] Please answer Yes or No.' ;;
-  esac
-done
-
-echo "done!!"
+echo "[test1] done!!"
