be6e3e94c64a79bccc8ae9ac2adb90a6992a5432
hiijima <42027082+iijima-hiroaki@users.noreply.github.com>
2022-01-13 21:37:53 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-echo $@
+while true; do
+  echo test1
+  read -p "[test1] please yes or no. (y/N): " yn
+  case $yn in
+    [Yy]* ) break ;;
+    [Nn]* ) printf $RED "[test1] 処理を中断します." && exit 1;;
+    * ) echo '[test1] Please answer Yes or No.' ;;
+  esac
+done
 
-echo $1
-echo $2
+while true; do
+  echo test2
+  read -p "[test2] please yes or no. (y/N): " yn
+  case $yn in
+    [Yy]* ) break ;;
+    [Nn]* ) printf $RED "[test2] 処理を中断します." && exit 1;;
+    * ) echo '[test2] Please answer Yes or No.' ;;
+  esac
+done
